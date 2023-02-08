8fbcc5ed9ad7bd95112b1ddcd94e047f580c2983
PriyathamVarma <priyatham002@gmail.com>
2023-02-08 23:33:33 +0000

    types branch done

--- README.md
+++ README.md
@@ -28,3 +28,21 @@ Packages group certain files into a group
 
 1. Executable
 2. Reusable : Helper
+
+
+## Variables
+
+
+## Arrays
+
+1. Arrays -> Fixed length
+2. Slice -> Slice is like a dynamic array
+
+
+## Slice range type
+
+fruits[startIndex:upToIndex]
+
+like, 
+- fruits[1:2] --> from 1 until 2
+- fruits[:2] --> until 2
--- hello.go
+++ hello.go
@@ -4,7 +4,16 @@ package main
 // Imports
 import "fmt"
 
+// types
+ var fruitsList []string
+
+// Slice range example
+// Normal slice or dynamic array
+fruits := fruitsList{"Apple","Orange","Promegenate","Carrot","Strawberry","Cherry","Dragon fruit"}
+
+
 // Functions
 func main() {
-	fmt.Println("hello world")
+	//fmt.Println("hello fruits")
+	fmt.Println(fruits)
 }
